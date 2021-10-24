7b12d2d97b5045c4f54518100f78676bd56f8695
SirMeows <araelbrighteyes@gmail.com>
2021-10-24 18:36:05 +0200

    add success-page.html email-list-page.html, changed EmailRequestController

--- src/main/java/com/example/freemoneynoscam/EmailDataBaseService.java
+++ src/main/java/com/example/freemoneynoscam/EmailDataBaseService.java
@@ -1,26 +1,41 @@
 package com.example.freemoneynoscam;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmailDataBaseService {
+    public Connection conn;
 
     public void insertEmailIntoDB(String input) {
-        var email = new Email(input);
-        if(email.isValidEmail()) {
+        var query = "INSERT INTO email ('email-address') VALUES(input)";
+        executeQuery(query);
+    }
 
-            try {
+    public List<String> getAllEmails() {
+        var list = new ArrayList<String>();
+        var query = "SELECT 'email-address' FROM email";
+
+
+        return list;
+    }
+
+    private void executeQuery(String query) {
+        try {
+            if(conn==null) {
                 var conn = createServerConnection();
-                var psts = conn.prepareStatement("INSERT INTO email ('email-address') VALUES(input)");
-                psts.execute();
-
-            } catch (SQLException e) {
-                System.out.println("Cannot connect to database");
-                e.printStackTrace();
             }
+
+            var psts = conn.prepareStatement(query);
+            psts.execute();
+
+        } catch (SQLException e) {
+            System.out.println("Cannot connect to database");
+            e.printStackTrace();
         }
     }
 
     private Connection createServerConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/emails","root","_FIFIfufu!495");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/emails","root","miip moop");
     }
 }
--- src/main/java/com/example/freemoneynoscam/controllers/EmailRequestController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/EmailRequestController.java
@@ -2,23 +2,30 @@ package com.example.freemoneynoscam.controllers;
 
 import com.example.freemoneynoscam.EmailDataBaseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class EmailRequestController {
+    EmailDataBaseService edbs = new EmailDataBaseService();
 
-    @GetMapping("/")
+    @GetMapping("/get-email")
     public String emailRequest() {
         return "email-request-page";
     }
 
-    @PostMapping("/user-email")
+    @PostMapping("/get-email")
     public String userEmail(WebRequest wr) {
         var parameter = wr.getParameter("email");
-        var edbs = new EmailDataBaseService();
         edbs.insertEmailIntoDB(parameter);
-        return "email-request-page";
+        return "success-page"; // supposed to redirect to avoid double submission
+    }
+
+    @GetMapping("display-emails")
+    public Object listOfEmails(Model model) {
+        model.addAttribute("emailList", edbs.getAllEmails());
+        return "email-list-page";
     }
 }
